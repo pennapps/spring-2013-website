f0d89462468c031c758b42d73a41155f2e7d2b76
Ceasar Bautista <cbautista2010@gmail.com>
2012-08-05 18:23:26 -0400

    Make nav CTA highlight on non-landing pages
    Just being consistent.

--- js/subnav.js
+++ js/subnav.js
@@ -1,14 +1,20 @@
-$(document).scroll(function(){
-    $masthead = $('#masthead button')
-    masthead_bottom = $masthead.offset().top
-    if ($(this).scrollTop() > masthead_bottom) {
-        $('#global-nav button').removeClass('btn-inverse');
-        $('#global-nav button').addClass('btn-primary');
-    } else {
-        $('#global-nav button').removeClass('btn-primary');
-        $('#global-nav button').addClass('btn-inverse');
-    }
-});
+$MASTHEAD = $('#masthead button');
+
+if ($MASTHEAD.length === 0) {
+    $('#global-nav button').removeClass('btn-inverse');
+    $('#global-nav button').addClass('btn-primary');
+} else {
+    $(document).scroll(function(){
+        masthead_bottom = $MASTHEAD.offset().top
+        if ($(this).scrollTop() > masthead_bottom) {
+            $('#global-nav button').removeClass('btn-inverse');
+            $('#global-nav button').addClass('btn-primary');
+        } else {
+            $('#global-nav button').removeClass('btn-primary');
+            $('#global-nav button').addClass('btn-inverse');
+        }
+    });
+}
 
 
 $('#global-nav').scrollspy()

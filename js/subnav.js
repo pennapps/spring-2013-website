dbad3e7b5d5699ced669ea24d467d24e0f1c91f1
Ceasar Bautista <cbautista2010@gmail.com>
2012-08-05 18:16:30 -0400

    Make nav CTA highlight once main CTA is offscreen
    Part technical trick, part being consistent with our CTA.

--- js/subnav.js
+++ js/subnav.js
@@ -1,1 +1,14 @@
+$(document).scroll(function(){
+    $masthead = $('#masthead button')
+    masthead_bottom = $masthead.offset().top
+    if ($(this).scrollTop() > masthead_bottom) {
+        $('#global-nav button').removeClass('btn-inverse');
+        $('#global-nav button').addClass('btn-primary');
+    } else {
+        $('#global-nav button').removeClass('btn-primary');
+        $('#global-nav button').addClass('btn-inverse');
+    }
+});
+
+
 $('#global-nav').scrollspy()
